81f62c707875d9b5d6841a8ce7efcc9c0394dd8b
liuxp0827 <liuxp_0325@qq.com>
2020-10-18 15:52:53 +0800

    add: mcrp/engine about engine and state machine

---
+++ engine/mrcp_recog_state_machine.go
@@ -0,0 +1,11 @@
+package engine
+
+import (
+	"github.com/navi-tt/go-mrcp/apr/memory"
+	"github.com/navi-tt/go-mrcp/mrcp"
+)
+
+/** Create MRCP recognizer state machine */
+func MRCPRecognizerStateMachineCreate(obj interface{}, version mrcp.Version, pool *memory.AprPool) *MRCPStateMachine {
+	return nil
+}
--- engine/mrcp_state_machine.go
+++ engine/mrcp_state_machine.go
@@ -8,12 +8,39 @@ type MRCPStateMachine struct {
 	Active bool        // State either active or deactivating
 
 	/** Virtual update */
-	Update func(machine *MRCPStateMachine, message *message.MRCPMessage) bool
+	Update func(machine *MRCPStateMachine, message *message.MRCPMessage) error
 	/** Deactivate */
-	Deactivate func(machine *MRCPStateMachine) bool
+	Deactivate func(machine *MRCPStateMachine) error
 
 	/** Message dispatcher */
-	OnDispatch func(machine *MRCPStateMachine, message *message.MRCPMessage) bool
+	OnDispatch func(machine *MRCPStateMachine, message *message.MRCPMessage) error
 	/** Deactivated */
-	OnDeactivate func(machine *MRCPStateMachine) bool
+	OnDeactivate func(machine *MRCPStateMachine) error
+}
+
+/** Initialize MRCP state machine */
+func MRCPStateMachineInit(sMachine *MRCPStateMachine, obj interface{}) error {
+	sMachine.obj = obj
+	sMachine.Active = true
+	sMachine.Update = nil
+	sMachine.Deactivate = nil
+	sMachine.OnDispatch = nil
+	sMachine.OnDeactivate = nil
+	return nil
+}
+
+/** Update MRCP state machine */
+func (m *MRCPStateMachine) MRCPStateMachineUpdate(message *message.MRCPMessage) error {
+	if m.Update != nil {
+		return m.Update(m, message)
+	}
+	return nil
+}
+
+/** Deactivate MRCP state machine */
+func (m *MRCPStateMachine) MRCPStateMachineDeactivate() error {
+	if m.Deactivate != nil {
+		return m.Deactivate(m)
+	}
+	return nil
 }
