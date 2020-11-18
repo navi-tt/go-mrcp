62598dca0e67c4eaf475e32005fcff07085084ba
liuxp0827 <liuxp_0325@qq.com>
2020-11-18 10:53:08 +0800

    实现 mpf audio file stream

--- mpf/mpf_audio_file_stream.go
+++ mpf/mpf_audio_file_stream.go
@@ -1,5 +1,67 @@
 package mpf
 
+import "os"
+
+/** Audio file stream */
+type AudioFileStream struct {
+	audioStream *AudioStream
+
+	readHandle  *os.File
+	writeHandle *os.File
+
+	eof          bool
+	maxWriteSize int64
+	curWriteSize int64
+}
+
+func AudioFileEventRaise(stream *AudioStream, eventId int, descriptor interface{}) {
+
+}
+
+func AudioFileDestroy(stream *AudioStream) error {
+	var (
+		fileStream *AudioFileStream = stream.Obj.(*AudioFileStream)
+	)
+	if fileStream.readHandle != nil {
+		err := fileStream.readHandle.Close()
+		if err != nil {
+			return err
+		}
+		fileStream.readHandle = nil
+	}
+	if fileStream.writeHandle != nil {
+		err := fileStream.writeHandle.Close()
+		if err != nil {
+			return err
+		}
+		fileStream.writeHandle = nil
+	}
+	return nil
+}
+
+func AudioFileReaderOpen(stream *AudioStream, codec *Codec) error {
+	return nil
+}
+
+func AudioFileReaderClose(stream *AudioStream) error {
+	return nil
+}
+
+func AudioFileFrameRead(stream *AudioStream, frame *Frame) error {
+	var (
+		fileStream *AudioFileStream = stream.Obj.(*AudioFileStream)
+	)
+	if fileStream.readHandle != nil && !fileStream.eof {
+		_, err := frame.CodecFrame.Buffer.ReadFrom(fileStream.readHandle)
+		if err != nil {
+			return err
+		}
+
+	}
+
+	return nil
+}
+
 /**
  * Create file stream.
  * @param termination the back pointer to hold
